a185571ea13d37b0841368360c9a9c7f8a9cbdb0
Michael <65642892+mikkyfly@users.noreply.github.com>
2025-01-22 10:21:14 +0500

    Force github

---
+++ src/main.ts
@@ -0,0 +1,58 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import {ConfigService} from '@nestjs/config';
+import * as cookieParser from 'cookie-parser';
+import IORedis from 'ioredis';
+import { ValidationPipe } from '@nestjs/common';
+import * as session from 'express-session';
+import { ms, StringValue } from './libs/common/utils/ms.util';
+import { parseBoolean } from './libs/common/utils/parse-booleab.until';
+import {RedisStore} from 'connect-redis';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+
+  const config = app.get(ConfigService);
+  const redis = new IORedis(config.getOrThrow('REDIS_URI'));
+
+  app.use(cookieParser(config.getOrThrow<string>('COOKIES_SECRET')));
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      transform:true
+    })
+  );
+
+  app.use(
+    session({
+      secret:config.getOrThrow<string>('SESSION_SECRET'),
+      name: config.getOrThrow<string>('SESSION_NAME'),
+      resave: true,
+      saveUninitialized: false,
+      cookie: {
+        domain: config.getOrThrow<string>('SESSION_DOMAIN'),
+        maxAge: ms(config.getOrThrow<StringValue>('SESSION_MAX_AGE')),
+        httpOnly: parseBoolean(
+          config.getOrThrow<string>('SESSION_HTTP_ONLY'),
+        ),
+        secure: parseBoolean(
+          config.getOrThrow<string>('SESSION_SECURE'),
+        ),
+        sameSite: 'lax'
+      },
+      store: new RedisStore({
+        client: redis,
+				prefix: config.getOrThrow<string>('SESSION_FOLDER')
+			})
+    })
+  );
+
+  app.enableCors({
+    origin: config.getOrThrow<string>('ALLOWED_ORIGIN'),
+    credentials: true,
+    exposedHeaders:['set-cookie']
+  });
+
+  await app.listen(config.getOrThrow<number>('APPLICATION_PORT'));
+}
+bootstrap();
